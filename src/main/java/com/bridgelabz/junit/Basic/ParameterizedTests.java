1e44abb9b1717dea3400272a1ebbe77c6e65f94d
VipulArora_2003 <vipularora9930@gmail.com>
2025-04-04 14:37:00 +0530

    [Vipul] Add: Introduced ParameterizedTests for multiple input testing

---
+++ src/main/java/com/bridgelabz/junit/Basic/ParameterizedTests.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.junit.Basic;
+
+public class ParameterizedTests {
+
+    public static boolean isEven(int num) {
+        if(num % 2 == 0){
+
+            return true;
+        }
+        return false;
+    }
+}
